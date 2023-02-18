3b60fb90d9a3ca23c2e3181d614e2cb45075f4b4
Jose Waiman <71891792+defnis@users.noreply.github.com>
2023-02-18 19:53:23 -0300

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,18 +1,61 @@
 <?php
-if(isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $phone = $_POST['phone'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Recoger los datos del formulario
+  $nombre = $_POST["nombre"];
+  $telefono = $_POST["telefono"];
+  $correo = $_POST["correo"];
+  $tema = $_POST["tema"];
+  $mensaje = $_POST["mensaje"];
 
-  $para = 'josewaiman@gmail.com';
-  $titulo = 'Mensaje desde el formulario de contacto';
-  $contenido = "Nombre: $nombre\nTeléfono: $telefono\nCorreo: $correo\nTema: $tema\nMensaje: $mensaje";
+  // Validar los datos
+  $errores = array();
+  if (empty($nombre)) {
+    $errores[] = "El nombre es obligatorio.";
+  }
+  if (empty($telefono)) {
+    $errores[] = "El número telefónico es obligatorio.";
+  }
+  if (empty($correo)) {
+    $errores[] = "La dirección de correo es obligatoria.";
+  } else if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+    $errores[] = "La dirección de correo no es válida.";
+  }
+  if (empty($tema)) {
+    $errores[] = "El tema es obligatorio.";
+  }
+  if (empty($mensaje)) {
+    $errores[] = "El mensaje es obligatorio.";
+  }
 
-  mail($para, $titulo, $contenido);
+  // Enviar el correo electrónico si no hay errores
+  if (empty($errores)) {
+    $destinatario = "josewaiman@gmail.com";
+    $asunto = "Nuevo mensaje de contacto";
+    $cuerpo = "Nombre: $nombre\n" .
+              "Teléfono: $telefono\n" .
+              "Correo electrónico: $correo\n" .
+              "Tema: $tema\n" .
+              "Mensaje:\n$mensaje";
+    $cabeceras = "From: $nombre <$correo>" . "\r\n" .
+                 "Reply-To: $correo" . "\r\n" .
+                 "X-Mailer: PHP/" . phpversion();
 
-  echo 'Mensaje enviado';
+    if (mail($destinatario, $asunto, $cuerpo, $cabeceras)) {
+      // Mostrar un mensaje de éxito
+      echo "<p>Su mensaje ha sido enviado correctamente. Me pondre en contacto con usted lo antes posible.</p>";
+    } else {
+      // Mostrar un mensaje de error
+      echo "<p>Lo siento, ha habido un error al enviar su mensaje. Por favor, inténtelo de nuevo más tarde.</p>";
+    }
+  } else {
+    // Mostrar los errores de validación
+    echo "<ul>";
+    foreach ($errores as $error) {
+      echo "<li>$error</li>";
+    }
+    echo "</ul>";
+  }
 }
 ?>
 
